c4c62d3e678908ac2fa336271c533f4dd18108c0
Prosper Emebo <emeboprosper@gmail.com>
2022-04-30 04:38:03 +0100

    Added Footer Section

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,19 @@
 const navigationEl = document.querySelector('.navigation');
 const mobileNavBtn = document.getElementById('nav-open');
 const mobileNavIcon = document.querySelector('#nav-open .burger');
+const dropdownElements = document.querySelectorAll('.drop-down');
+const allLinks = document.querySelectorAll('a:link');
 
 // Mobile Navigation Responsive Nav
+const toggleNavigation = () => {
+  navigationEl.classList.toggle('open');
+  mobileNavIcon.classList.toggle('open');
+};
+
 mobileNavBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
-  navigationEl.classList.toggle('open');
-  mobileNavIcon.classList.toggle('open');
+  toggleNavigation();
 });
 
 // Swiper
@@ -28,10 +34,64 @@ const swiper = new Swiper('.swiper-container', {
     modifier: 1.5,
     slideShadows: false,
   },
+});
 
-  //   // Navigation arrows
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   },
+// Drop Down Component
+
+const dropdownFunc = (dropEv) => {
+  let dropDownCard = dropEv.target.closest('.drop-down');
+  let content = dropEv.target.closest('.drop-down').querySelector('.content');
+  let btn = dropEv.target.closest('.dropdown-toggle');
+
+  if (
+    (!dropEv.target.matches('.custom-link') &&
+      dropDownCard.dataset.open === 'self') ||
+    btn
+  ) {
+    dropDownCard.classList.toggle('open');
+
+    if (content.offsetHeight === 0) {
+      content.style.height = content.scrollHeight + 'px';
+    } else {
+      content.style.height = '0px';
+    }
+  }
+};
+
+dropdownElements.forEach((dropDown) => {
+  dropDown.addEventListener('click', (event) => dropdownFunc(event));
+});
+
+// Navigation Scroll to Section
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    const href = link.getAttribute('href');
+
+    if (href.includes('/') || href.includes('.html')) {
+      return;
+    }
+
+    // Scroll back to top
+    if (href === '#') {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      e.preventDefault();
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains('nav-item-link')) {
+      e.preventDefault();
+
+      toggleNavigation();
+    }
+  });
 });
